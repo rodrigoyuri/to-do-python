d9db5ea98a81543ec8dc51ddf379cf1cbf8a7936
Rodrigo Yuri <rodrigo33996@gmail.com>
2021-01-13 15:30:44 -0300

    access control

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.core.paginator import Paginator
 from django.contrib import messages
@@ -7,6 +8,7 @@ from .forms import TaskForm
 from .models import Task
 # Create your views here.
 
+@login_required
 def taskList(request):
     
     search = request.GET.get('search')
@@ -24,10 +26,12 @@ def taskList(request):
     
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
+@login_required
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
 
+@login_required
 def newTask(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -43,7 +47,8 @@ def newTask(request):
     else:
         form = TaskForm()
         return render(request, 'tasks/addtask.html', {'form': form}) 
-        
+
+@login_required
 def editTask(request, id):
     task = get_object_or_404(Task, pk=id)
     form = TaskForm(instance=task)
@@ -59,7 +64,8 @@ def editTask(request, id):
             
     else:
         return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
-
+        
+@login_required
 def deleteTask(request, id):
     task = get_object_or_404(Task, pk=id)
     task.delete()
