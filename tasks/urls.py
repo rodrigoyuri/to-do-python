a826e266951d476609588cf93e810cb5f75ab72c
Rodrigo Yuri <rodrigo33996@gmail.com>
2021-01-13 12:14:12 -0300

    routes and views Task

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('helloworld/', views.helloWorld),
+    path('', views.taskList, name="task-list"),
+    path('task/<int:id>', views.taskView, name="task-view"),
 ]
