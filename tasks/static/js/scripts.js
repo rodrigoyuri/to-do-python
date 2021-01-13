a9ad548fb95b354c58da64a01a66785cac4ec2cc
Rodrigo Yuri <rodrigo33996@gmail.com>
2021-01-13 14:21:06 -0300

    comfirmation alert

--- tasks/static/js/scripts.js
+++ tasks/static/js/scripts.js
@@ -1,1 +1,15 @@
-console.log('Functiona');
+$(document).ready(function() {
+
+    var deleteBtn = $('.delete-btn')
+
+    $(deleteBtn).on('click', function(el) {
+        el.preventDefault()
+
+        let deleteLink = $(this).attr('href')
+        let result = confirm('Quer deletar esta tarefa?')
+
+        if(result) {
+            window.location.href = deleteLink
+        }
+    })
+})
